bd54bbbe1be9b8ee2643a30f29cb725276c91cf5
Ginny Mangat <ginnymangat@Ginnys-MacBook-Air.local>
2018-08-28 00:26:07 -0700

    configured SSL and the Puma webserver in production

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :username, :admin
-  has_many :attendances, dependent: :destroy
+  has_many :attendances
 	has_many :meetups, through: :attendances, dependent: :destroy
 	has_many :created_meetups, foreign_key: "creator_id", class_name: "Meetup", dependent: :destroy
 
